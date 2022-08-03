24fbf081fb96d874a6ecc33d785141da58e85470
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2022-08-03 02:48:03 +0200

    Switch to Zig because Zigma Male Gigachad Lang

--- src/main.rs
+++
@@ -1,7 +0,0 @@
-use kagero::printer::{Printer, Colors};
-
-fn main() {
-    let mut printer = Printer::default();
-    printer.println("Hello, world!", Colors::Red);
-    printer.println("This will be a very cool build system in the future!", Colors::CyanBright);
-}
