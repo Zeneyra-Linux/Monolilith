f5ef26e143464ad7a88b1d799d020187ebc9d58e
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2022-08-16 20:37:55 +0200

    Fix typo

--- src/meta.rs
+++ src/meta.rs
@@ -4,7 +4,7 @@ const NAME_RICH: &str = "Monolilith";
 const REPOSITORY: &str = env!("CARGO_PKG_REPOSITORY");
 const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
 const LICENSE: &str = "EUPL-1.2";
-const LICENSE_RICH: &str = "European Union Pulic License 1.2";
+const LICENSE_RICH: &str = "European Union Public License 1.2";
 
 // Build Script Metadata
 const CARGO_VERSION: &str = env!("CARGO_VERSION");
