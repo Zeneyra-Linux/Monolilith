0eeefdd10f3b6605785e01bb8f7a21a5d938a852
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2022-08-24 11:10:08 +0200

    List files now works properly!

--- src/compile/mod.rs
+++ src/compile/mod.rs
@@ -28,34 +28,62 @@ pub mod clang;
 pub mod go;
 
 
-/// List Files by Extension
+/// List Directory Entries
 /// 
-/// Lists all files in a directory by extension.
-/// Used for things like C and Go compiling where all root files have to be set.
-pub fn list_files(dir: impl AsRef<Path>, ext: &str) -> io::Result<()> {
+/// List Directory Entries as [Metadata](fs::Metadata) in an [Iterator]
+pub fn list_meta(dir: impl AsRef<Path>) -> io::Result<impl Iterator<Item = (fs::DirEntry, fs::Metadata)>> {
     // Read Directory and Iterate over Files
-    let files = fs::read_dir(dir)?.into_iter()
-
-    // Filter Out unavailable entries and non-files
+    let entries = fs::read_dir(dir)?.into_iter()
+    // Filter Out unavailable entries
     .filter_map(|entry| {
         // Check if entry is available
         if let Ok(direntry) = entry {
             // Check if entry's metadata is available
             if let Ok(filemeta) = direntry.metadata() {
-                // Check if entry is a file
-                if filemeta.is_file() {
-                    return Some(filemeta);
-                }
+                return Some((direntry, filemeta));
             }
         }
         None
     });
-    Ok(())
+    Ok(entries)
 }
 
-/// List Files by Extension recursively
-/// 
-/// Lists all files in a directory and its subdirectories by extension.
-pub fn list_files_recursive(dir: impl AsRef<Path>, ext: &str) {
 
+/// List Files by Extension
+/// 
+/// Lists all files in a directory by extension.
+/// Used for things like C and Go compiling where all root files have to be set.
+/// 
+/// `ext` should be the file extension WITHOUT the dot, e.g. `rs`, `go`, `c`, etc.
+/// 
+/// # Example
+/// ```
+/// fn main() {
+///     for ele in compile::list_files("./src", "rs").unwrap() {
+///         println!("{}", ele);
+///     }
+/// }
+/// ``` 
+pub fn list_files(dir: impl AsRef<Path>, ext: &str) -> io::Result<Vec<String>> {
+    let files_iter = list_meta(dir)?
+    // Filter out non-files
+    .filter(|x| x.1.is_file())
+    // Filter by extension
+    .filter(|y| {
+        if let Some(extension) = y.0.path().extension() {
+            if let Ok(extend) = extension.to_owned().into_string() {
+                return extend.ends_with(ext);
+            }
+        }
+        false
+    });
+
+    // Cast iter to Vec<String> containing the file names
+    Ok(files_iter.filter_map(|x| {
+        // Filter by successful conversion from OsString to String
+        if let Ok(filename) = x.0.file_name().into_string() {
+            return Some(filename);
+        }
+        None
+    }).collect())
 }
